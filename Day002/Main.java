a6d52ec8771f1d3050cc03c5a06ce56eb41a8a41
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-09-09 22:02:13 +0800

    Commit

---
+++ Day002/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Day002;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String nama = "Abrar"; // %s tipe data String
+       int umur = 18; // %d bilangan bulat
+       double bb = 45.056; // %.2f bilangan desimal (boleh digunankan 3f juga)
+       
+       System.out.printf("nama %s umur %d berat badan %.2f", nama, umur, bb);
+        System.out.print("nama saya " + nama);
+        System.out.println("umur " + umur);
+            
+    }
+}
