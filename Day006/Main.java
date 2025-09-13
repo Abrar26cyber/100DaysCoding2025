b552d4e02fb575589f02b2cb2923f71c6bca3478
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-09-13 20:07:22 +0800

    commit

---
+++ Day006/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Day006;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //pemanggilan tipe data bilangan desimal 
+         
+        //pemanggilan tipe data float dengan 32 bit
+        float tb = 162.2f;
+        
+        //pemanggilan tipe data double dengan 64 bit
+        double bb = 45.68;
+        
+        //memunculkan dengan println untuk membuat baris baru
+        System.out.println("Tinggi badan:" + tb);
+        System.out.println("Berat badan:" + bb);
+    }
+}
