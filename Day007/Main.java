8c86165e9c90096d81392f97663e4c1a8741c54c
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-09-14 19:29:54 +0800

    commit

---
+++ Day007/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Day007;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //pemannggilan tipe data karakter 
+        
+        //pemanggilan tipe data Char yang menyimpan karakter tunggal, seperti huruf, angka, atau simbol dengan tanda petik 1 ('')
+        char karakter = 'A';
+        
+        //pemanggilan tipa data karakter yang menyimpan nilai logika dan hanya memmiliki dua  kemungkinan true dan false
+        boolean benar = true;
+        
+        System.out.println("Inisial:" + karakter);
+        System.out.println("Apakah A adaalah ininsial mu? " + benar);
+        
+    }
+    
+}
