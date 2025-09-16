4fedcd66b4e7c7a181138236a9ef5d057efcddde
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-09-16 22:06:05 +0800

    commit

---
+++ Day009/Main.java
@@ -0,0 +1,20 @@
+
+package Day009;
+
+public class Main {
+    public static void main(String[] args) {
+        //Deklarasi,  inisialisasi dan update variabel
+        String nama;// deklarasi mendefinisikan variabel dan menentukan tipe data
+        nama = "Abrar";// inisialisasi variabel : memberikan nilai/value pada variabel
+        int umur;
+        umur = 17;
+        System.out.println("Nama:" + nama);
+        System.out.println("Umur:" + umur);
+        
+        nama = "Muh.Abrar";//update variabel yakni mengubah nilai atau value pada variabel
+        umur = 18;
+        System.out.println("Nama:" + nama);
+        System.out.println("Umur:" + umur);
+        
+    }
+}
