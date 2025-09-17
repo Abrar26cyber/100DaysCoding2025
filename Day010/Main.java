8fed14e3911cc187f377248dd77fbd3bdda23112
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-09-17 17:03:09 +0800

    commit

---
+++ Day010/Main.java
@@ -0,0 +1,17 @@
+
+package Day010;
+
+public class Main {
+    public static void main(String[] args) {
+        // pengggunaan konstanta / final pada tipe data
+        final String nama = "Abrar";
+        final int gabungan = 18;
+        // final pada sebuah variabel nilai atau valuenya tidak akan berubah lagi
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("Maksimal Gabungan : " + gabungan);
+        
+        
+    }
+    
+}
