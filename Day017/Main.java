2a41b4a9f479f5de4a38a5239f5aca585b1979fe
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-09-24 11:21:03 +0800

    commit

---
+++ Day017/Main.java
@@ -0,0 +1,38 @@
+package Day017;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        //operator aritmatika sisa bagi atau modulus (%)
+        Scanner masuk = new Scanner (System.in);
+        
+        System.out.print("Masukkan Angka \t\t : ");
+        int angkaA = masuk.nextInt();
+        
+        System.out.print("Masukkan operator \t : ");
+        char operator = masuk.next().charAt(0);
+        
+        System.out.print("Masukkan Angka \t\t : ");
+        int angkaB = masuk.nextInt();
+        
+        double hasilBagi;
+        double modulus = 0;
+        
+        if(operator == '/'){
+            hasilBagi = angkaA / angkaB;
+            modulus  = angkaA % angkaB;
+        }if(modulus  != 0){
+            hasilBagi = angkaA / angkaB;
+            modulus  = angkaA % angkaB;
+            System.out.println("Hasil bagi \t\t : " + hasilBagi);
+            System.out.println("Modulus \t\t : " +  modulus);
+        }else{
+            hasilBagi = angkaA / angkaB;
+            System.out.println("Hasil bagi \t\t : " + hasilBagi);
+            System.out.println("tidak ada sisa bagi dari hasil pembagian atau moodulusnya = " + modulus);
+        }
+        
+ 
+    }
+    
+}
