69df36da89e6c22c3d07bf5cd9ce54fae62a66eb
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-09-26 08:19:35 +0800

    commit

---
+++ Day019/Main.java
@@ -0,0 +1,27 @@
+package Day019;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //konversi manual/dipaksa (darri tipe data besar ke kecil)
+        Scanner in = new Scanner (System.in); 
+        
+        System.out.print("Masukkan angka \t\t : ");
+        double angka1 = in.nextDouble();
+        int konversi1 = (int)angka1;
+        
+        System.out.print("Masukkan \t\t : ");
+        float angka2 = in.nextFloat();
+        byte konversi2 = (byte)angka2;
+        
+        System.out.println();
+        System.out.println("nilai dari variabel denggan tipe data short \t\t : " + angka1);
+        System.out.println("konversi ke tipe data long \t\t\t\t : " + konversi1);
+        System.out.println();
+        System.out.println("nilai dari variabel denggan tipe data integer \t : " + angka2);
+        System.out.println("konversi ke tipe data byte \t\t\t\t : " + konversi2);
+       
+    }  
+}
