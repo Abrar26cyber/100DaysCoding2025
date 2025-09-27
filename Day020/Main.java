c49ebb1fa1ae861ec6961559abeaa6874f85c8fc
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-09-27 20:09:02 +0800

    commit

--- Day020/Main.java
+++ Day020/Main.java
@@ -2,26 +2,35 @@ package Day020;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //mengubahh tipe data String ke tipe data primitif
+        //mengubahh tipe data Primitif ke tipe data
         Scanner masuk = new Scanner (System.in);
         System.out.print("Masukkan input ke 1 \t : ");
-        String str = masuk.nextLine();
+        int angkaInt = masuk.nextInt();
+        String str = String.valueOf(angkaInt);
+        
         System.out.print("Masukkan input ke 2 \t : ");
-        String stc =  masuk.nextLine();
+        long angkaLong =  masuk.nextLong();
+        String stc = String.valueOf(angkaLong);
         
-        Integer konversi1 = Integer.valueOf(str);
-        Byte konversi2 = Byte.valueOf(stc);
-        Short konversi3 = Short.valueOf(str);
-        Long konversi4 = Long.valueOf(stc);
-        Double konversi5 = Double.valueOf(str);
-        Float konversi6 = Float.valueOf(stc);
+        System.out.print("Masukkan input ke 3 \t : ");
+        float angkaFloat =  masuk.nextFloat();
+        String std = String.valueOf(angkaFloat);
         
-        System.out.println("Hasil konversi String 1 ke Integer \t : " + konversi1);
-        System.out.println("Hasil konversi String 2 ke byte \t : " + konversi2);
-        System.out.println("Hasil konversi String 1 ke short \t : " + konversi3);
-        System.out.println("Hasil konversi String 2 ke long \t : " + konversi4);
-        System.out.println("Hasil konversi String 1 ke double \t : " + konversi5);
-        System.out.println("Hasil konversi String 2 ke float \t : " + konversi6);
+        System.out.print("Masukkan input ke 4 \t : ");
+        double angkaDouble =  masuk.nextDouble();
+        String stb= String.valueOf(angkaDouble);
+        
+        System.out.println();
+        
+        System.out.println("Value dari tipe data Integer \t\t : " + angkaInt);
+        System.out.println("Hasil konversi Integer ke String \t : " + str);
+        System.out.println("Value dari tipe data long \t\t : " + angkaLong);
+        System.out.println("Hasil konversi long ke String \t\t : " + stc);
+        System.out.println("Value dari tipe data float \t\t : " + angkaFloat);
+        System.out.println("Hasil konversi float ke String \t : " + std);
+        System.out.println("Value dari tipe data double \t\t : " + angkaDouble);
+        System.out.println("Hasil konversi double ke String \t : " + stb);
+ 
         
     }
 }
