157174babc8c8b7984471597d1aadba3f588f9c9
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-10-02 14:02:15 +0800

    commit

---
+++ Day025/Main.java
@@ -0,0 +1,35 @@
+package Day025;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Masukkan angka \t : ");
+        int angka = in.nextInt();
+        
+        System.out.println("\n===INCREMENT===");
+        int angka1 = angka;
+        System.out.println("Bentuk awal \t\t= "+ angka1);
+        System.out.println("Bentuk prefix \t\t= "+(++angka1));
+        System.out.println("nilai setelah prefix \t= " + angka1);
+        System.out.println();
+        angka1 = angka;
+        System.out.println("Bentuk awal \t\t= "+ angka1);
+        System.out.println("Bentuk postfix \t\t= "+(angka1++));
+        System.out.println("nilai setelah postfix \t= " + angka1);
+       
+        System.out.println("\n===DECCREMENT===");
+        angka1 = angka;
+        System.out.println("Bentuk awal \t\t= "+ angka1);
+        System.out.println("Bentuk prefix \t\t= "+(--angka1));
+        System.out.println("nilai setelah prefix \t= " + angka1);
+        System.out.println();
+        angka1 = angka;
+        System.out.println("Bentuk awal \t\t= "+ angka1);
+        System.out.println("Bentuk postfix \t\t= "+(angka1--));
+        System.out.println("nilai setelah postfix \t= " + angka1);
+    }
+    
+}
