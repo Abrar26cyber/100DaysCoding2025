4136bbe23e9f1bfc39147c1d276aca78acea63f6
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-10-08 20:58:36 +0800

    commit

---
+++ Day031/Main.java
@@ -0,0 +1,21 @@
+package Day031;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+         Scanner masuk = new Scanner (System.in);
+         
+         System.out.println("Masukkan angka");
+         int ab = masuk.nextInt();
+         
+         boolean if_1 = ab > 0 && ab < 9; 
+         boolean if_2 = ab >= 10 && ab < 99 ; 
+         boolean if_3 = ab >= 100 && ab < 999 ; 
+         
+         System.out.println("Bilangan Satuan\t: " + if_1);
+         System.out.println("Bilangan puluhan\t: " + if_2);
+         System.out.println("Bilangan Ratusan\t: " + if_3);
+    }
+}
