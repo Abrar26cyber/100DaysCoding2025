6e98e58c01c860e9b5b8a818e5eaf0426e91f1cf
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-11-04 21:58:52 +0800

    commit

---
+++ Day058/Main.java
@@ -0,0 +1,23 @@
+package Day058;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Masukkan Angka N: ");
+        int n = in.nextInt();
+        System.out.println("===== 1 - N =====");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+
+        }
+        System.out.println("\n===== N-1 =====");
+        for (int i = n; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
