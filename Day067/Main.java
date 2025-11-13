18b867c73d18602965c425bd1da9834efe7b8d9e
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-11-13 23:17:35 +0800

    commit

---
+++ Day067/Main.java
@@ -0,0 +1,18 @@
+package Day067;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Masukkan nama: ");
+        String kata = in.nextLine();
+        
+        for (int i = 1; i <= kata.length(); i++) {
+            System.out.print(kata.charAt(2)+ " ");
+        }
+        System.out.println();
+    }
+}
