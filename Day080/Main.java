3c57280229db306baa0bebc5f7bf345abb3499ad
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-11-26 22:13:59 +0800

    commit

---
+++ Day080/Main.java
@@ -0,0 +1,12 @@
+package Day080;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String [] nama = {"Nisa","kayla","Windah","Yusuf","Asrul"};
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println((i+1)+ ". " + nama[i]);        }
+    }
+
+}
