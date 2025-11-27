74b4bb30dbf7b2d514dde614519d5aaf9886123f
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-11-27 14:41:09 +0800

    commit

---
+++ Day081/Main.java
@@ -0,0 +1,24 @@
+package Day081;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        
+        String [] nama = new String [5];
+        nama [0] = "Abrar";
+        nama [1] = "Asrul";
+        nama [2] = "Asriadi";
+        nama [3] = "yusuf";
+        nama [4] = "Ariel";
+        
+        System.out.println("indeks 0. " +nama[0]);
+        System.out.println("indeks 1. " +nama[1]);
+        System.out.println("indeks 2. " +nama[2]);
+        System.out.println("indeks 3. " +nama[3]);
+        System.out.println("indeks 4. " +nama[4]);
+    }
+
+}
