dd74a364f4fecb06712e7e07b83e8b0ea27bf0d8
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-12-04 22:23:06 +0800

    commit

---
+++ Day088/Main.java
@@ -0,0 +1,15 @@
+package Day088;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String [] pulau = {"Sumatra","Jawa","Sulawesi","Kalimantan","Papua"};
+        
+        System.out.println("==============================================");
+        for(String a : pulau){
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+}
