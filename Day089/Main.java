dba8cfc333d52c6f5243154e4e4a24492b3fbf2e
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-12-05 21:39:42 +0800

    commit

---
+++ Day089/Main.java
@@ -0,0 +1,13 @@
+package Day089;
+
+public class Main {
+
+    static void sapa() {
+        System.out.println("Selamat malam para pengguna java!!!!!");
+    }
+
+    public static void main(String[] args) {
+        sapa();
+    }
+
+}
