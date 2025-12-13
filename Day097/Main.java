e4b8ec48d41a9f03a1c9449890e50c2e3e6b632b
Abrar26cyber <Syaputraabrar15@gmail.com>
2025-12-13 11:51:27 +0800

    commit

--- Day097/Main.java
+++ Day097/Main.java
@@ -1,27 +1,31 @@
-
 package Day097;
+
 import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+
         System.out.print("Masukkan panjang persegi\t:");
         double p = in.nextDouble();
-        System.out.print("Masukkan Luas persegi\t\t:");
+        System.out.print("Masukkan Lebar persegi\t\t:");
         double l = in.nextDouble();
         System.out.println("==============================================");
         if (p == l) {
             double luas = luas(p, l);
             System.out.printf("Luas Persegi = %.1f %n", luas);
-        }else{
-            double total = luasP(p ,l);
+        } else {
+            double total = luasP(p, l);
             System.out.printf("Luas Persegi panjang = %.1f %n", total);
         }
     }
-    static double luasP (double a, double b){
+
+    static double luasP(double a, double b) {
         return a * b;
     }
-    static double luas (double a, double b){
+
+    static double luas(double a, double b) {
         return a * b;
     }
 }
